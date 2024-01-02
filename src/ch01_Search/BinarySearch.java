f1ac16e444c06a3b5b43c10ca91e111df8b2f839
jdohun <wjdehgns97@naver.com>
2024-01-02 21:32:53 +0900

    feat: BinarySearch 구현
    이진 탐색 알고리즘을 구현하고 실행합니다.

---
+++ src/ch01_Search/BinarySearch.java
@@ -0,0 +1,43 @@
+package ch01_Search;
+
+public class BinarySearch {
+    public static int search(final int[] arr, final int target) {
+        int first = 0;
+        int last = arr.length;
+        int mid;
+
+        while (first <= last) {
+            mid = (first + last) / 2;
+
+            if (target == arr[mid]) {
+                return mid;
+            } else {
+                if (target < arr[mid]) {
+                    last = mid - 1; // -1 하는 이유 : 이미 비교한 mid 인덱스를 다시 비교하지 않기 위해
+                } else {
+                    first = mid + 1; // +1 하는 이유 : 또한 mid 인덱스의 값이 target 이라면 무한 루프가 형성되기 때문에
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void test() {
+        int[] arr = {1, 3, 5, 7, 9};
+
+        int idx = search(arr, 7);
+        if (-1 == idx) {
+            System.out.println("탐색 실패\n");
+        } else {
+            System.out.printf("타겟 저장 인덱스 : %d\n", idx);
+        }
+
+        idx = search(arr, 4);
+        if (-1 == idx) {
+            System.out.println("탐색 실패\n");
+        } else {
+            System.out.printf("타겟 저장 인덱스 : %d\n", idx);
+        }
+    }
+}
