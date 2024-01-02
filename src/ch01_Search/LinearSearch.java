400b65cd091e4c1241598e5672a33bdca4a92131
jdohun <wjdehgns97@naver.com>
2024-01-02 21:30:58 +0900

    feat: LinearSearch 구현
    선형 탐색 알고리즘을 구현하고 실행합니다.

---
+++ src/ch01_Search/LinearSearch.java
@@ -0,0 +1,30 @@
+package ch01_Search;
+
+public class LinearSearch {
+    public static int search(final int[] arr, final int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void test() {
+        int[] arr = {3, 5, 2, 4, 9};
+
+        int idx = search(arr, 4);
+        if (-1 == idx) {
+            System.out.println("탐색 실패\n");
+        } else {
+            System.out.printf("타겟 저장 인덱스 : %d\n", idx);
+        }
+
+        idx = search(arr, 7);
+        if (-1 == idx) {
+            System.out.println("탐색 실패\n");
+        } else {
+            System.out.printf("타겟 저장 인덱스 : %d\n", idx);
+        }
+    }
+}
