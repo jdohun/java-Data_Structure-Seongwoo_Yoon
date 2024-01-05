44ddab08da52e7589b40668cd89642628097004d
jdohun <wjdehgns97@naver.com>
2024-01-05 20:25:00 +0900

    feat: Add testCall in Fibonacci
    피보나치 함수 호출 순서를 확인하는 기능 구현

--- src/ch02_Recursive/Fibonacci.java
+++ src/ch02_Recursive/Fibonacci.java
@@ -2,6 +2,8 @@ package ch02_Recursive;
 
 public class Fibonacci {
     public static int fibonacci(int n) {
+        System.out.printf("func call param %d\n", n);
+
         if (n == 1) {
             return 0;
         } else if (n == 2) {
@@ -15,5 +17,9 @@ public class Fibonacci {
         for (int i = 1; i < 15; i++) {
             System.out.printf("%d ", fibonacci(i));
         }
+    }
+
+    public static void testCall() {
+        fibonacci(7);
     }
 }
