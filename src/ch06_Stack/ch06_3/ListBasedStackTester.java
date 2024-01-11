1e30fc7bc73bfbf71a6f1f1f67e4aa2222af65ab
jdohun <wjdehgns97@naver.com>
2024-01-12 00:25:21 +0900

    docs: ListBasedStack 주석 추가

--- src/ch06_Stack/ch06_3/ListBasedStackTester.java
+++ src/ch06_Stack/ch06_3/ListBasedStackTester.java
@@ -2,15 +2,17 @@ package ch06_Stack.ch06_3;
 
 public class ListBasedStackTester {
     public static void test() {
+        // Stack의 생성 및 초기화 ///////
         ListBasedStack stack = new ListBasedStack();
 
+        // 데이터 넣기 ///////
         stack.push(1);
         stack.push(2);
         stack.push(3);
         stack.push(4);
         stack.push(5);
 
-
+        // 데이터 꺼내기 ///////
         while (!stack.empty()) {
             System.out.printf("%d ", stack.pop());
         }
