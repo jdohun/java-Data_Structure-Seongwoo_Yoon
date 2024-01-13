25d92e0d066bb24653371bf6eb88fd7633f1081b
jdohun <wjdehgns97@naver.com>
2024-01-13 20:01:45 +0900

    feat: ch07_2 배열 기반 큐 구현 및 테스트 수행

---
+++ src/ch07_Queue/ch07_2/ArrayBasedCircularQueue.java
@@ -0,0 +1,57 @@
+package ch07_Queue.ch07_2;
+
+import ch07_Queue.ch07_1.QueueADT;
+
+public class ArrayBasedCircularQueue<E> implements QueueADT<E> {
+    private static int QUEUE_LEN = 100;
+    int front = 0;
+    int rear = 0;
+    Object[] queueArr = new Object[QUEUE_LEN];
+
+    @Override
+    public boolean empty() {
+        if (front == rear)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public void enqueue(E data) {
+        final int next = nextPositionIndex(rear);
+        if (next == front) {
+            System.out.println("Queue is Full");
+            System.exit(-1);
+        }
+        rear = next;
+        queueArr[rear] = data;
+    }
+
+    @Override
+    public E dequeue() {
+        if (empty()) {
+            System.out.println("Queue is Empty");
+            System.exit(-1);
+        }
+        front = nextPositionIndex(front);
+
+        // 굳이 삭제(초기화)를 하지 않아도 삭제로 간주한다.
+        return (E) queueArr[front];
+    }
+
+    @Override
+    public E peek() {
+        if (empty()) {
+            System.out.println("Queue is Empty");
+            System.exit(-1);
+        }
+        return (E) queueArr[nextPositionIndex(front)];
+    }
+
+    private int nextPositionIndex(int position) {
+        if (position == QUEUE_LEN - 1)
+            return 0;
+        else
+            return position + 1;
+    }
+}
---
+++ src/ch07_Queue/ch07_2/ArrayBasedCircularQueueTester.java
@@ -0,0 +1,19 @@
+package ch07_Queue.ch07_2;
+
+public class ArrayBasedCircularQueueTester {
+    public static void test() {
+        // Queue의 생성 및 초기화 ///////
+        ArrayBasedCircularQueue<Integer> queue = new ArrayBasedCircularQueue<>();
+
+        // 데이터 넣기 ///////
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+
+        // 데이터 꺼내기 ///////
+        while (!queue.empty())
+            System.out.printf("%d ", queue.dequeue());
+    }
+}
