7a18a2b87b25e47c8f17fdc1f688cce39e3778e5
jdohun <wjdehgns97@naver.com>
2024-01-30 20:41:12 +0900

    feat: 선택 정렬 구현

---
+++ src/ch10_Sort/ch10_1_SimpleSort/ch10_02_SelectionSort/SelectionSort.java
@@ -0,0 +1,34 @@
+package ch10_Sort.ch10_1_SimpleSort.ch10_02_SelectionSort;
+
+public class SelectionSort {
+    static void SelectionSort(int[] arr) {
+        int currentIndex;
+        int temp;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            currentIndex = i;
+
+            // 최솟값 탐색
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[currentIndex]) {
+                    currentIndex = j;
+                }
+            }
+
+            // 교환
+            temp = arr[i];
+            arr[i] = arr[currentIndex];
+            arr[currentIndex] = temp;
+        }
+    }
+
+    public static void test() {
+        int[] arr = {3, 2, 4, 1};
+
+        SelectionSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%d ", arr[i]);
+        }
+    }
+}
