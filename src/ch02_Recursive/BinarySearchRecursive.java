1e45f03f7aa1877cab08d56e3aa4c293e48e9dcc
jdohun <wjdehgns97@naver.com>
2024-04-17 19:48:55 +0900

    refactor: 불필요한 class 제거 및 BinarySearchRecursive 의 search 를 searchRecursive 로 이름 변경

--- src/ch02_Recursive/BinarySearchRecursive.java
+++ src/ch02_Recursive/BinarySearchRecursive.java
@@ -1,7 +1,7 @@
 package ch02_Recursive;
 
 public class BinarySearchRecursive {
-    public static int search(final int[] arr, final int first, final int last, final int target) {
+    public static int searchRecursive(final int[] arr, final int first, final int last, final int target) {
         int mid;
 
         if (first > last) return -1;
@@ -11,9 +11,9 @@ public class BinarySearchRecursive {
             return mid;
         } else {
             if (target < arr[mid]) {
-                return search(arr, first, mid - 1, target);
+                return searchRecursive(arr, first, mid - 1, target);
             } else {
-                return search(arr, mid + 1, last, target);
+                return searchRecursive(arr, mid + 1, last, target);
             }
         }
     }
@@ -22,14 +22,14 @@ public class BinarySearchRecursive {
         int[] arr = {1, 3, 5, 7, 9};
         int idx;
 
-        idx = search(arr, 0, arr.length - 1, 7);
+        idx = searchRecursive(arr, 0, arr.length - 1, 7);
         if (-1 == idx) {
             System.out.println("탐색 실패\n");
         } else {
             System.out.printf("타겟 저장 인덱스 : %d\n", idx);
         }
 
-        idx = search(arr, 0, arr.length - 1, 4);
+        idx = searchRecursive(arr, 0, arr.length - 1, 4);
         if (-1 == idx) {
             System.out.println("탐색 실패\n");
         } else {
